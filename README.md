94de20742c4b52d2ba7a2a5bb6d5d6ae507649cf
Henri Bakker <henribakker@apples-MacBook-Air.local>
2022-02-13 23:09:17 +0800

    initial project set up commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-hello test test
+PROJECT:
+Create a script that can simulate multiple trades between x amount of people. When someone reaches 0 funds they are out. The goal is to prove that that at some point 1 person will be left with all the money.
+
+TO DO:
+-Add logic for making trades.
+-Add logic for removing a trader that reached 0.
+
+LOG:
+--- Sunday February 13th, 2022 ---
+Started the project, created the classes, etc.
+
--- script.py
+++ script.py
@@ -1,1 +1,24 @@
-print("Hello")
+class Trader:
+    def __init__(self, name, funds):
+        self.name = name
+        self.funds = funds
+        #trader_list.append(name)
+
+trader_list = []
+
+piet = Trader("Piet", 1000)
+klaas = Trader("Klaas", 2000)
+joris = Trader("Joris", 3000)
+henri = Trader("Henri", 4000)
+deja = Trader("Deja", 5000)
+
+trader_list.append(piet)
+trader_list.append(klaas)
+trader_list.append(joris)
+trader_list.append(henri)
+trader_list.append(deja)
+
+print(trader_list[1].name)
+
+def make_trade(trader1, trader2):
+	print(f"{trader1.name} trades with {trader2.name}")
