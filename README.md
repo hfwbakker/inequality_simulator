12e4e1d8a5f0fc2c72e2748fb6e80943105dcf92
Henri Bakker <henribakker@apples-MacBook-Air.local>
2022-03-07 22:43:17 +0800

    round 2

--- README.md
+++ README.md
@@ -2,10 +2,13 @@ PROJECT:
 Create a script that can simulate multiple trades between x amount of people. When someone reaches 0 funds they are out. The goal is to prove that that at some point 1 person will be left with all the money.
 
 TO DO:
--Add logic for making trades.
+-Add logic for looping over trades until all but 1 reached 0. Count loops. (e.g. while len(list) > 1: )
 -Add logic for removing a trader that reached 0.
 
 LOG:
+--- Monday March 7th, 2022 ---
+- Wrote logic for making trades and increasing / decreasing amounts between random traders.
+
 --- Sunday February 13th, 2022 ---
 Started the project, created the classes, etc.
 
--- script.py
+++ script.py
@@ -1,10 +1,25 @@
-class Trader:
-    def __init__(self, name, funds):
-        self.name = name
-        self.funds = funds
-        #trader_list.append(name)
+import random
 
-trader_list = []
+class Trader:
+	def __init__(self, name, funds):
+		self.name = name
+		self.funds = funds
+		trader_list.append(self.name)
+
+	def check_money(self):
+		print(f"{self.name} has {self.funds} muney left")
+	
+	def gain(self, amount):
+		#print(f"{self.name} earned {amount}")
+		self.funds += amount	
+		print(f"{self.name} funds are now {self.funds}")
+
+	def lose(self, amount):
+		#print(f"{self.name} lost {amount}")
+		self.funds -= amount
+		#print(f"{self.name}'s funds decreased to {self.funds}")
+
+
 
 piet = Trader("Piet", 1000)
 klaas = Trader("Klaas", 2000)
@@ -12,13 +27,44 @@ joris = Trader("Joris", 3000)
 henri = Trader("Henri", 4000)
 deja = Trader("Deja", 5000)
 
-trader_list.append(piet)
-trader_list.append(klaas)
-trader_list.append(joris)
-trader_list.append(henri)
-trader_list.append(deja)
 
-print(trader_list[1].name)
+object_list = [piet, klaas, joris, henri, deja]
+
 
 def make_trade(trader1, trader2):
-	print(f"{trader1.name} trades with {trader2.name}")
+	print(f"{trader1.name}'s current funds are {trader1.funds}. {trader2.name}'s funds are {trader2.funds}.")
+	amount = random.randint(100, 1000)
+	print(f"amount to be traded is {amount}.")
+	trader1.lose(amount/2)
+	print(f"{trader1.name} and {trader2.name} both invested {amount/2}")
+	trader2.lose(amount/2)
+	trader1_luck = random.randint(0,100)/100
+	trader1_result = round(amount * trader1_luck)
+	print(f"{trader1.name}'s return is {trader1_result}. {trader2.name}'s return is {amount - trader1_result}.")
+	trader1.gain(trader1_result)
+	trader2.gain(round(amount - trader1_result))
+
+
+
+def random_trade(list):
+	trader1_num = random.randint(0, len(list)-1)
+	trader1 = list[trader1_num]
+	print(trader1.name)
+	
+	trader2_num = trader1_num
+	while trader2_num == trader1_num:
+		trader2_num = random.randint(0,len(list)-1)
+	trader2 = list[trader2_num]
+	print(trader2.name)
+	
+	make_trade(trader1, trader2)	
+
+
+def remove_trader(trader_name):
+	trader_list.remove(trader_name)	
+
+random_trade(object_list)
+
+
+
+
